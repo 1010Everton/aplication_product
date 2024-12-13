1acdeddeaeebe2215ed67d6d08d381d182c04394
EvErToN <evertonflorenciodasilva@outlook.com>
2024-12-12 21:40:14 -0300

    tive que criar tudo de novo ja que o onedrive apagou tudo da minha area de trabalho

---
+++ src/main/java/com/example/demo/controler/Controler.java
@@ -0,0 +1,13 @@
+package com.example.demo.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controler {
+
+    @GetMapping("/")
+    public void cadastro(){
+
+    }
+}
