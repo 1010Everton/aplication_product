2afe2add7e06fd940d2dec77205919e2e7e301cd
EvErToN <evertonflorenciodasilva@outlook.com>
2024-12-16 21:34:27 -0300

    criei o metodo para inserir as informações do produto e seus getters para cessar as variaveis

--- src/main/java/com/example/demo/produtos/cadastro_produto.java
+++ src/main/java/com/example/demo/produtos/cadastro_produto.java
@@ -3,12 +3,47 @@ package com.example.demo.produtos;
 import javax.xml.crypto.Data;
 
 public class cadastro_produto {
+
     private String nome;
     private String empresa;
-    private Float serie;
+    private Float Serie;
     private Data data;
     private Data Fabricacao;
     private Data Vencimento;
+
+
+    public cadastro_produto(String nome, String empresa, Float Serie, Data Fabricacao, Data Vencimento){
+
+    this.nome=nome;
+    this.empresa=empresa;
+    this.Serie=Serie;
+    this.Fabricacao=Fabricacao;
+    this.Vencimento=Vencimento;
+
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public Float getSerie() {
+        return Serie;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public Data getFabricacao() {
+        return Fabricacao;
+    }
+
+    public Data getVencimento() {
+        return Vencimento;
+    }
 
 
 }
