fc33e94caa2acb2317f4da76bd93f61f883f5e42
EvErToN <evertonflorenciodasilva@outlook.com>
2024-12-16 21:16:01 -0300

    criando variaveis para receberem os valores do produto

---
+++ src/main/java/com/example/demo/produtos/cadastro_produto.java
@@ -0,0 +1,14 @@
+package com.example.demo.produtos;
+
+import javax.xml.crypto.Data;
+
+public class cadastro_produto {
+    private String nome;
+    private String empresa;
+    private Float serie;
+    private Data data;
+    private Data Fabricacao;
+    private Data Vencimento;
+
+
+}
