5804638070f07023415fc2a8f03838773285f253
EvErToN <evertonflorenciodasilva@outlook.com>
2025-01-21 21:14:30 -0300

    criei a classe entidade

--- src/main/java/com/example/demo/Acesso_Ao_Banco/Entidade.java
+++ src/main/java/com/example/demo/Acesso_Ao_Banco/Entidade.java
@@ -1,11 +1,41 @@
 package com.example.demo.Acesso_Ao_Banco;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @jakarta.persistence.Entity
-@Table(name="")
+@Table(name="usuarios")
 public class Entidade{
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String senha;
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
 
