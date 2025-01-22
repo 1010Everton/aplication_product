881b7d990d8262634188acc13e285172057f8fd9
EvErToN <evertonflorenciodasilva@outlook.com>
2025-01-21 21:21:58 -0300

    criei a interface repository

--- src/main/java/com/example/demo/Acesso_Ao_Banco/repository.java
+++ src/main/java/com/example/demo/Acesso_Ao_Banco/repository.java
@@ -1,4 +1,9 @@
 package com.example.demo.Acesso_Ao_Banco;
 
-public class repository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface repository extends JpaRepository<Entidade,Long> {
 }
