43fa9694458b0409a0e8fff67784dafb228733ae
EvErToN <evertonflorenciodasilva@outlook.com>
2025-02-02 12:19:26 -0300

    consegui intergrar o scene builder no codigo

--- Interface_Visual/src/main/java/com/example/interface_visual/HelloApplication.java
+++ Interface_Visual/src/main/java/com/example/interface_visual/HelloApplication.java
@@ -2,34 +2,21 @@ package com.example.interface_visual;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloApplication extends Application {
 
     @Override
-    public void start(Stage stage) {
-        // Criando os componentes da interface
-        Label senhaLabel = new Label("Senha");
-        Label welcomeText = new Label("Login");
-        Button confirmarButton = new Button("Confirmar");
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/PainelGrafico.fxml"));
+        Parent root = loader.load();
 
-        // Adicionando ação ao botão
-        confirmarButton.setOnAction(event -> welcomeText.setText("Botão pressionado!"));
-
-        // Criando o layout VBox
-        VBox vbox = new VBox(20, senhaLabel, welcomeText, confirmarButton);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setPadding(new Insets(20));
-
-        // Criando a cena e configurando o palco
-        Scene scene = new Scene(vbox, 280, 308);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
--- Interface_Visual/src/main/java/com/example/interface_visual/HelloController.java
+++ Interface_Visual/src/main/java/com/example/interface_visual/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.interface_visual;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label campoLogin;
+    @FXML
+    private Label campoSenha;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick(ActionEvent event) {
+        String Login = campoLogin.getText();
+        String Senha = campoSenha.getText();
+
     }
 }
