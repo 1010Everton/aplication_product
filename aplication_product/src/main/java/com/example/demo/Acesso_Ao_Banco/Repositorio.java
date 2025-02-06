24489c6ed6ed90e1955484191e767bd51052394b
EvErToN <evertonflorenciodasilva@outlook.com>
2025-02-05 22:48:33 -0300

    implementei a logica para atraves do formulario html enviar para o banco de dados os valores de login e senha

--- aplication_product/src/main/java/com/example/demo/Acesso_Ao_Banco/Repositorio.java
+++ aplication_product/src/main/java/com/example/demo/Acesso_Ao_Banco/Repositorio.java
@@ -2,9 +2,10 @@ package com.example.demo.Acesso_Ao_Banco;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Repositorio extends JpaRepository<Entidade_Login, Long> {
+public interface Repositorio extends CrudRepository<Entidade_Login, Long> {
 
 }
--- aplication_product/src/main/java/com/example/demo/controler/Controlador_Login.java
+++ aplication_product/src/main/java/com/example/demo/controler/Controlador_Login.java
@@ -3,6 +3,9 @@ package com.example.demo.controler;
 import com.example.demo.Acesso_Ao_Banco.Entidade_Login;
 
 import com.example.demo.Acesso_Ao_Banco.Repositorio;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +13,17 @@ import org.springframework.web.bind.annotation.*;
 
 
 
-@RestController
-@RequestMapping("/inserir")
+@Controller
 public class Controlador_Login {
 
     @Autowired
     Repositorio rep;
 
 
-    @PostMapping
-    public @ResponseBody Entidade_Login inserirdados(@RequestParam String login, @RequestParam String senha) {
-        Entidade_Login ent = new Entidade_Login(login, senha);
-        ent.setLogin(login);
-
-        ent.setSenha(senha);
-        rep.save(ent);
-        return ent;
+    @PostMapping("/inserir")
+    public ResponseEntity<String> inserirUsuario(@RequestBody Entidade_Login usuario) {
+        rep.save(usuario);
+        return ResponseEntity.ok("{\"message\": \"Usuário cadastrado com sucesso!\"}");
     }
     @GetMapping("/")
     public String home(Model model) {
